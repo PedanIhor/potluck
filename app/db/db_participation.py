74cee7faf49285c9a9c5ad483f783e1374669fd1
Ihor Pedan <ihorpedan@gmail.com>
2025-02-15 20:52:11 +0100

    Added Add dishes request

--- app/db/db_participation.py
+++ app/db/db_participation.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm.session import Session
 
 from app.db.schemas import ParticipationBase
-from app.db.models import Participation
+from app.db.models import Participation, Dish
 
 
 def get_participations_for_party(db: Session, party_id: int):
@@ -15,3 +15,13 @@ def create_participation(db: Session, participation: ParticipationBase):
     db.commit()
     db.refresh(participation_model)
     return participation_model
+
+
+def add_dishes_to_participation(db: Session, participation_id: int, dishes_ids: list[int]):
+    participation = db.query(Participation).filter(Participation.id == participation_id).first()
+    dishes = db.query(Dish).filter(Dish.id.in_(dishes_ids)).all()
+    print(dishes)
+    participation.dishes.extend(dishes)
+    db.commit()
+    db.refresh(participation)
+    return participation
--- app/routers/participations.py
+++ app/routers/participations.py
@@ -24,3 +24,9 @@ def create_participation(request: ParticipationBase, db = Depends(get_db), curre
 def read_participations(party_id: int, db = Depends(get_db), current_user: CurrentUser = Depends(get_current_user)):
     return db_participation.get_participations_for_party(db, party_id)
 
+
+@router.post("/{participation_id}/dishes")
+def add_dishes_to_participation(participation_id: int, dishes_ids: list[int], db = Depends(get_db), current_user: CurrentUser = Depends(get_current_user)):
+    if not current_user.admin and current_user.id != db_participation.get_participations_for_party(db, participation_id)[0].user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    return db_participation.add_dishes_to_participation(db, participation_id, dishes_ids)
